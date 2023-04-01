1a8274faa871dafad2d005b1afdf854058d79a79
Edgar <edgarmedinafuentes@gmail.com>
2023-04-01 17:51:43 -0600

    Imprementacion de localstorage y formato JSON

--- js/index.js
+++ js/index.js
@@ -34,6 +34,65 @@ class Carousel{
     }
 }
 
+class Articulos{
+    constructor(nom, id, tipo, precio){
+        this.nom = nom;
+        this.id = id;
+        this.tipo = tipo;
+        this.precio = precio;
+    }
+}
+
 const carousel = document.querySelectorAll('[data-carousel]');
 carousel.forEach(carousel => new Carousel(carousel));
+
+const termo = [
+    new Articulos("Termo Mate", "A17188", "ACRILICO", 160),
+    new Articulos("Glitter Star", "A010232-MET", "ACRILICO", 180), 
+    new Articulos("Vaso Fiest", "VA1565", "PLASTICO", 30)
+];
+
+
+const articulosNom = document.querySelectorAll('.nombreArticulo');
+const articulosP = document.querySelectorAll('.precio');
+
+articulosNom.forEach((art, i) => {
+    art.innerText = termo[i].nom
+});
+
+articulosP.forEach((art, i) => {
+    art.innerText = "$"+termo[i].precio
+});
+
+console.log(articulosP[1]);
+
+function Agregar(){
+    const data = JSON.stringify(termo[0]);
+    console.log(data);
+    localStorage.setItem('1',data);
+}
+
+function Agregar2(){
+    const data = JSON.stringify(termo[1]);
+    console.log(data);
+    localStorage.setItem('2',data);
+}
+
+function Agregar3(){
+    const data = JSON.stringify(termo[2]);
+    console.log(data);
+    localStorage.setItem('3',data);
+}
+
+const agregarCarrito1 = document.querySelector('[btnAgregarCarrito1]');
+
+agregarCarrito1.addEventListener('click', Agregar);
+
+const agregarCarrito2 = document.querySelector('[btnAgregarCarrito2]');
+
+agregarCarrito2.addEventListener('click', Agregar2);
+
+const agregarCarrito3 = document.querySelector('[btnAgregarCarrito3]');
+
+agregarCarrito3.addEventListener('click', Agregar3);
 
